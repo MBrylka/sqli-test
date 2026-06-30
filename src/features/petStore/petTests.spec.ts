aebf30255b69f9a092f3ce1690af0bd888ab0a4c
Maciej Brylka <maciejbrylka94@gmail.com>
2026-06-30 09:31:08 +0200

    move displaying pet tuples and uniqe names into separate methods

--- src/features/petStore/petTests.spec.ts
+++ src/features/petStore/petTests.spec.ts
@@ -5,16 +5,27 @@ import { PetData } from "./models/PetModel";
 import { ExClass } from "./excercise/ExClass";
 
 
+function soldPets(pets: PetData[]): void {
+    pets.forEach((pet: PetData) => {
+        console.log({id: pet.id, name: pet.name})
+        test.expect(pet.status).toBe("sold")
+    })
+}
+
+function displayUniquePetNames(names: { name: string; amount: number; }[]): void {
+        names.forEach(({name, amount}) => {
+            console.log({name, amount})
+        })
+}
+
+
 test.describe('Pet store api tests', async () => {
     test('Find pets by status', async ({request}) => {
         const petStoreClient = new PetStoreClient(request)
         const petStoreActions = new PetStoreActions(petStoreClient)
         const response = await petStoreActions.findPetsByStatus("sold")
         const responseJson = await response.json()
-        responseJson.forEach((pet: PetData) => {
-            console.log({id: pet.id, name: pet.name})
-            test.expect(pet.status).toBe("sold")
-        })
+        soldPets(responseJson)
     })
 
     test('Display unique pet names', async ({request}) => {
@@ -24,8 +35,6 @@ test.describe('Pet store api tests', async () => {
         const responseJson = await response.json()
         const exClass = new ExClass(responseJson)
         const namesCount = exClass.countUniqueNames()
-        namesCount.forEach(({name, amount}) => {
-            console.log({name, amount})
-        })
+        displayUniquePetNames(namesCount)
     })
 })
